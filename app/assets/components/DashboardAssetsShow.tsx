6a6f9ed59d03385bd0a23f531d1464ca9dda8215
batuhan içöz <batuhan@icoz.co>
2022-03-17 14:39:21 +0300

    fix tabs state

--- app/assets/components/DashboardAssetsShow.tsx
+++ app/assets/components/DashboardAssetsShow.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect } from "react"
+import { Suspense, useEffect, useState } from "react"
 import { Head, Link, useRouter, useQuery, useParam, BlitzPage, useMutation, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import deleteAsset from "../mutations/deleteAsset"
@@ -27,12 +27,12 @@ import {
 import { Tab } from "@headlessui/react"
 
 function Demo() {
-  const [state, handlers] = useAccordionState({ total: 3, initialItem: 0 })
+  const [activeTab, setActiveTab] = useState(0)
   const theme = useMantineTheme()
   const breakpoints = [{ maxWidth: "sm" as const, cols: 1 }]
 
   return (
-    <Tabs state={state} onTapChange={handlers.setState} disableIconRotation multiple>
+    <Tabs active={activeTab} onTabChange={setActiveTab}>
       <Tabs.Tab label="Overview" icon={<CardStackPlusIcon color={theme.colors.blue[6]} />}>
         <SimpleGrid cols={2} breakpoints={breakpoints}>
           <TextInput label="Email" placeholder="Email" required />
@@ -46,7 +46,7 @@ function Demo() {
           autosize
         />
         <Group position="right" mt="xl">
-          <Button onClick={() => handlers.toggle(1)}>Next step</Button>
+          <Button onClick={() => setActiveTab(1)}>Next step</Button>
         </Group>
       </Tabs.Tab>
       <Tabs.Tab label="Deliverables" icon={<CircleIcon color={theme.colors.red[6]} />}>
@@ -67,10 +67,10 @@ function Demo() {
         />
 
         <Group position="apart" mt="xl">
-          <Button variant="default" onClick={() => handlers.toggle(0)}>
+          <Button variant="default" onClick={() => setActiveTab(0)}>
             Previous step
           </Button>
-          <Button onClick={() => handlers.toggle(2)}>Next step</Button>
+          <Button onClick={() => setActiveTab(2)}>Next step</Button>
         </Group>
       </Tabs.Tab>
 
@@ -90,10 +90,10 @@ function Demo() {
         />
 
         <Group position="apart" mt="xl">
-          <Button variant="default" onClick={() => handlers.toggle(0)}>
+          <Button variant="default" onClick={() => setActiveTab(0)}>
             Previous step
           </Button>
-          <Button onClick={() => handlers.toggle(2)}>Next step</Button>
+          <Button onClick={() => setActiveTab(2)}>Next step</Button>
         </Group>
       </Tabs.Tab>
       <Tabs.Tab label="Danger Zone" icon={<CircleIcon color={theme.colors.cyan[6]} />}>
@@ -102,7 +102,7 @@ function Demo() {
           We will start processing your order soon
         </Text>
         <Group position="left" mt="xl">
-          <Button variant="default" onClick={() => handlers.toggle(1)}>
+          <Button variant="default" onClick={() => setActiveTab(1)}>
             Previous step
           </Button>
         </Group>
